5a98a2399bff26a5733f0b57ff6cb55dcc777926
banotraore <banotraore@gmail.com>
2019-05-01 16:02:26 +0200

    maj

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ApplicationRecord
   has_many :users
+  has_many :gossips, through: :users 
   validates :name, length: { in: 2..200 }, presence: true
   validates :zip_code, length: { in: 2..50 }, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'user/:id', to: 'user#user'
-  get 'welcome/:user_entry', to: 'welcome#first_name'
-  get '/home', to: 'home#home'
-  get '/team', to: 'team#team'
-  get '/contact', to: 'contact#contact'
-  resources :gossip
+  #get 'user/:id', to: 'user#user'
+  #get 'welcome/:user_entry', to: 'welcome#first_name'
+#  get '/team', to: 'team#team'
+  #get '/contact', to: 'contact#contact'
+  resources :gossip #ok
+  resources :home
+  root 'home#index'
+  resources :welcome
+  resources :contact
+  resources :user
+  resources :team
+
+
+
 end
