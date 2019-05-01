9931dc0a33c8d37d1186498c44eac13fa0add302
banotraore <banotraore@gmail.com>
2019-05-01 11:15:14 +0200

    maj routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/home', to: 'home#home'
   get '/team', to: 'team#team'
   get '/contact', to: 'contact#contact'
-  get '/gossip/:id', to: 'gossip#gossip'
+  resources :gossip
 end
